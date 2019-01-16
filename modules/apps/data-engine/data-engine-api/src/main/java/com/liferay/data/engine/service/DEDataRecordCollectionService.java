d33f881ac6f69a0a593ce3e83a19634a9af19c13
Leonardo Barros <leonardo.barros@liferay.com>
2019-01-16 08:53:44 -0300

    LPS-89168 Add new method to retrieve a data record collection

--- modules/apps/data-engine/data-engine-api/src/main/java/com/liferay/data/engine/service/DEDataRecordCollectionService.java
+++ modules/apps/data-engine/data-engine-api/src/main/java/com/liferay/data/engine/service/DEDataRecordCollectionService.java
@@ -64,6 +64,11 @@ public interface DEDataRecordCollectionService {
 				deDataRecordCollectionDeleteRequest)
 		throws DEDataRecordCollectionException;
 
+	
+	public DEDataRecordCollectionGetResponse execute(
+			DEDataRecordCollectionGetRequest deDataRecordCollectionGetRequest)
+		throws DEDataRecordCollectionException;
+
 	/**
 	 * Execute the Save Model Permissions Request which can grant permission
 	 * to a role to perform actions involving a Data Record Collection model
