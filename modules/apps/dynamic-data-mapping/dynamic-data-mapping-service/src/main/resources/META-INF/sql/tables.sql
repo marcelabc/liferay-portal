1fe42bea563949f25442db90a56932f8f7a05f53
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2019-02-04 12:22:18 -0300

    LPS-87625 build service

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/resources/META-INF/sql/tables.sql
@@ -151,7 +151,9 @@ create table DDMStructureLayout (
 	createDate DATE null,
 	modifiedDate DATE null,
 	structureVersionId LONG,
-	definition TEXT null
+	definition TEXT null,
+	description TEXT null,
+	name TEXT null
 );
 
 create table DDMStructureLink (
