6ce046a30d27bd1fe246791ff34bd08834312fc7
marcelabc <mbc3@cin.ufpe.br>
2019-04-09 15:51:10 -0300

    LPS-93630 Add new methods to support when a custom property is a Map

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/field/type/v1_0/util/CustomPropertyUtil.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/field/type/v1_0/util/CustomPropertyUtil.java
@@ -98,6 +98,22 @@ public class CustomPropertyUtil {
 		return Long.valueOf(0);
 	}
 
+	public static Map<String, String> getMap(
+		CustomProperty[] customProperties, String key) {
+
+		if (ArrayUtil.isEmpty(customProperties)) {
+			return new HashMap<>();
+		}
+
+		for (CustomProperty customProperty : customProperties) {
+			if (StringUtils.equals(key, customProperty.getKey())) {
+				return (Map<String, String>)customProperty.getValue();
+			}
+		}
+
+		return new HashMap<>();
+	}
+
 	public static String getString(
 		CustomProperty[] customProperties, String key) {
 
@@ -118,6 +134,36 @@ public class CustomPropertyUtil {
 		}
 
 		return defaultValue;
+	}
+
+	public static JSONObject toJSONObject(Map<String, String> stringMap)
+		throws Exception {
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		if (stringMap.isEmpty()) {
+			return jsonObject;
+		}
+
+		for (Map.Entry<String, String> stringEntry : stringMap.entrySet()) {
+			jsonObject.put(stringEntry.getKey(), stringEntry.getValue());
+		}
+
+		return jsonObject;
+	}
+
+	public static Map<String, String> toMap(JSONObject jsonObject) {
+		Map<String, String> values = new HashMap<>();
+
+		Iterator<String> keys = jsonObject.keys();
+
+		while (keys.hasNext()) {
+			String key = keys.next();
+
+			values.put(key, jsonObject.getString(key));
+		}
+
+		return values;
 	}
 
 }
