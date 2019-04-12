760cf60e950e4f0cbfb4e337ed25850e4ab9354c
marcelabc <mbc3@cin.ufpe.br>
2019-04-12 15:39:44 -0300

    LPS-93862 Create FieldTypeFactory

---
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/field/type/v1_0/FieldTypeFactory.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.rest.internal.field.type.v1_0;
+
+import com.liferay.data.engine.rest.dto.v1_0.DataDefinitionField;
+import com.liferay.portal.template.soy.data.SoyDataFactory;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author Marcela Cunha
+ */
+public class FieldTypeFactory {
+
+	public static void includeFieldTypeContext(
+		DataDefinitionField dataDefinitionField, String fieldType,
+		Map<String, Object> fieldTypeContext,
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse, boolean readOnly,
+		SoyDataFactory soyDataFactory) {
+
+		if (StringUtils.equals(fieldType, "captcha")) {
+			CaptchaFieldType.includeContext(
+				fieldTypeContext, dataDefinitionField, httpServletRequest,
+				httpServletResponse, readOnly, soyDataFactory);
+		}
+		else if (StringUtils.equals(fieldType, "checkbox")) {
+			CheckboxFieldType.includeContext(
+				fieldTypeContext, dataDefinitionField, httpServletRequest,
+				httpServletResponse, readOnly);
+		}
+		else if (StringUtils.equals(fieldType, "checkbox_multiple")) {
+			CheckboxMultipleFieldType.includeContext(
+				fieldTypeContext, dataDefinitionField, httpServletRequest,
+				httpServletResponse, readOnly);
+		}
+		else if (StringUtils.equals(fieldType, "date")) {
+			DateFieldType.includeContext(
+				fieldTypeContext, dataDefinitionField, httpServletRequest,
+				httpServletResponse, readOnly);
+		}
+		else if (StringUtils.equals(fieldType, "document_library")) {
+			DocumentLibraryFieldType.includeContext(
+				fieldTypeContext, dataDefinitionField, httpServletRequest,
+				httpServletResponse, readOnly);
+		}
+		else if (StringUtils.equals(fieldType, "editor")) {
+			EditorFieldType.includeContext(
+				fieldTypeContext, dataDefinitionField, httpServletRequest,
+				httpServletResponse, readOnly);
+		}
+	}
+
+}
