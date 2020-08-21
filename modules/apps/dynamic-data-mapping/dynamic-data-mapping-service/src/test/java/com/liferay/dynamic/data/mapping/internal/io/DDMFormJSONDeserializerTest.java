f343ef6fd4a18c021f764e398a82d14ba2ae85df
Marcela Cunha <marcela.cunha@liferay.com>
2020-08-21 13:24:36 -0300

    LPS-119125 Add test to verify the serialize/deserialize of DDMForm

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/test/java/com/liferay/dynamic/data/mapping/internal/io/DDMFormJSONDeserializerTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/test/java/com/liferay/dynamic/data/mapping/internal/io/DDMFormJSONDeserializerTest.java
@@ -44,6 +44,7 @@ import java.util.ResourceBundle;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import org.mockito.Matchers;
 import org.mockito.Mock;
@@ -63,6 +64,14 @@ public class DDMFormJSONDeserializerTest
 
 		setUpDDMFormJSONDeserializer();
 		setUpPortalUtil();
+	}
+
+	@Test
+	public void testDDMFormDeserializationWithSchemaVersion() throws Exception {
+		DDMForm ddmForm = deserialize(
+			read("ddm-form-json-deserializer-with-schema-version.json"));
+
+		Assert.assertEquals("2.0", ddmForm.getSchemaVersion());
 	}
 
 	@Override
