33c76fe9affacf4045c9ef70f2c9afe8d743373a
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2023-08-17 20:05:17 -0300

    LPS-193812 Only allowed bundles can manage system object relationships

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -345,6 +345,11 @@ public class ObjectServiceUpgradeStepRegistrator
 			"8.2.0", "8.3.0",
 			new com.liferay.object.internal.upgrade.v8_3_0.
 				ObjectValidationRuleUpgradeProcess());
+
+		registry.register(
+			"8.3.0", "8.4.0",
+			new com.liferay.object.internal.upgrade.v8_4_0.
+				ObjectRelationshipUpgradeProcess());
 	}
 
 	@Reference
