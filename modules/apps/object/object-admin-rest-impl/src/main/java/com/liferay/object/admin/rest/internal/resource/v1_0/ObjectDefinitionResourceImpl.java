54a8b2c6eeba3be660fd047878b3cdbc2b715bb4
Guilherme Sa <guilherme.sa@liferay.com>
2023-09-05 11:18:00 -0300

    LPS-193812 Allow update of labels for system object relationships

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
@@ -563,6 +563,9 @@ public class ObjectDefinitionResourceImpl
 				_objectValidationRuleLocalService.getObjectValidationRules(
 					objectDefinitionId));
 
+		List<ObjectRelationship> objectRelationships = ListUtil.fromArray(
+			objectDefinition.getObjectRelationships());
+
 		if (serviceBuilderObjectDefinition.isModifiable() &&
 			serviceBuilderObjectDefinition.isSystem() &&
 			ObjectDefinitionUtil.isInvokerBundleAllowed()) {
@@ -574,6 +577,10 @@ public class ObjectDefinitionResourceImpl
 				serviceBuilderObjectField ->
 					serviceBuilderObjectField.isMetadata() ||
 					!serviceBuilderObjectField.isSystem());
+
+			objectRelationships.removeIf(
+				objectRelationship -> !GetterUtil.getBoolean(
+					objectRelationship.getSystem()));
 			serviceBuilderObjectValidationRules.removeIf(
 				serviceBuilderObjectValidationRule ->
 					!serviceBuilderObjectValidationRule.isSystem());
@@ -585,6 +592,10 @@ public class ObjectDefinitionResourceImpl
 			serviceBuilderObjectFields.removeIf(ObjectFieldModel::isSystem);
 			serviceBuilderObjectValidationRules.removeIf(
 				ObjectValidationRuleModel::isSystem);
+
+			objectRelationships.removeIf(
+				objectRelationship -> GetterUtil.getBoolean(
+					objectRelationship.getSystem()));
 		}
 
 		for (ObjectField objectField : objectFields) {
@@ -644,12 +655,10 @@ public class ObjectDefinitionResourceImpl
 			_objectLayoutLocalService.deleteObjectLayouts(objectDefinitionId);
 		}
 
-		ObjectRelationship[] objectRelationships =
-			objectDefinition.getObjectRelationships();
-
 		Set<String> accountEntryRestrictedObjectRelationshipsNames =
 			_getAccountEntryRestrictedObjectRelationshipsNames(
-				serviceBuilderObjectDefinition, objectRelationships);
+				serviceBuilderObjectDefinition,
+				objectRelationships.toArray(new ObjectRelationship[0]));
 
 		ObjectValidationRule[] objectValidationRules =
 			objectDefinition.getObjectValidationRules();
@@ -672,7 +681,9 @@ public class ObjectDefinitionResourceImpl
 
 		_addObjectDefinitionResources(
 			accountEntryRestrictedObjectRelationshipsNames, objectActions,
-			objectDefinitionId, objectLayouts, objectRelationships,
+			serviceBuilderObjectDefinition.getObjectDefinitionId(),
+			objectLayouts,
+			objectRelationships.toArray(new ObjectRelationship[0]),
 			objectValidationRules, objectViews);
 
 		return _toObjectDefinition(serviceBuilderObjectDefinition);
